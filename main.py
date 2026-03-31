d8ecea0d950867b474b4d00a2b29840cef707926
Drum Holliday <107374333+PurpleBird3@users.noreply.github.com>
2026-03-31 14:58:59 -0500

    Part C: cleaned the dataset, handled missing values, converted dates to duration, encoded (translated) category values into numnerial format, implemented Random Forest model

--- main.py
+++ main.py
@@ -1,7 +1,94 @@
-# Testing to make sure Python works in IntelliJ
-print("Python is working")
 
-x = 5
-y = 10
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
 
-print("Test sum:", x + y)
+# Load the dataset
+data = pd.read_csv("data/Hospital_Staffing_dataset.csv")
+
+# Check missing values BEFORE cleaning
+print("\nMissing values per column:")
+print(data.isnull().sum())
+
+print("\nTotal missing values:")
+print(data.isnull().sum().sum())
+
+# Show the first five rows
+print(data.head())
+
+# SHow columns
+print("\nColumns:")
+print(data.columns)
+
+# Show info
+print("\nInfo:")
+print(data.info())
+
+#________
+# 1. CLEAN DATA
+# _______
+
+# Drop Facility Number
+data = data.drop(columns=["Facility Number"])
+
+# Fill all missing values since ML can not handle missing values (Data Imputation)
+data = data.fillna(0)
+
+#_________
+# 2. Convert Dates
+#_________
+
+# Convert dates columns to datetime since Random Forest can not read strings
+#data["Begin Date"] = pd.to_datetime(data["Begin Date"])
+#data["End Date"] = pd.to_datetime(data["End Date"])
+
+# Convert date columns to datetime (ignore invalid values like 0)
+data["Begin Date"] = pd.to_datetime(data["Begin Date"], errors='coerce')
+data["End Date"] = pd.to_datetime(data["End Date"], errors='coerce')
+
+# Create Duration column (difference in days)
+data["Duration"] = (data["End Date"] - data["Begin Date"]).dt.days
+# Fill missing duration values
+data["Duration"] = data["Duration"].fillna(0)
+
+# Drop Original Date Columns
+data = data.drop(columns=["Begin Date", "End Date"])
+
+#_________
+# 3. Define Target and Features
+#_________
+
+# Target (predictions)
+y = data["Productive Hours per Adjusted Patient Day"]
+
+# Features
+x = data.drop(columns=["Productive Hours per Adjusted Patient Day"])
+
+#__________
+# 4. Convert Categorical Data
+#__________
+
+# Convert text columns into numerical values
+x = pd.get_dummies(x)
+
+#____________
+# 5. Split Data (Standard Default of Test Size 20%, Training Size 80%)
+# Random State- A fixed random state used to ensure reproducibility of results.
+#___________
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=3
+)
+
+#________
+# 6. Train Model
+#________
+
+model = RandomForestRegressor(random_state=3)
+model.fit(x_train, y_train)
+
+#______
+# Make Predictions
+#______
+predictions = model.predict(x_test)
+print("\nSample Predictions: ")
+print(predictions[:10])
